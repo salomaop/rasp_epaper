c46e40c379be2ba93f9bc957bc5ea6b9208023fd
Salomao Prestupa Garbim <salo@mp9.dev>
2021-08-29 00:23:28 -0300

    New

--- epaper.py
+++ epaper.py
@@ -1,12 +1,12 @@
-import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'rasp_epaper/pic')
-print(picdir)
-
-import logging
 from lib.waveshare_epd import epd2in13b_V3
 import time
 from PIL import Image,ImageDraw,ImageFont
 import socket
+import os
+
+
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'rasp_epaper/pic')
+
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -24,6 +24,7 @@ def run():
         epd.init()
         # epd.Clear()
         time.sleep(1)
+
         font20 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 20)
         font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
 
@@ -32,15 +33,14 @@ def run():
         drawblack = ImageDraw.Draw(HBlackimage)
         drawred = ImageDraw.Draw(HRYimage)
         msg = "IP: " + get_ip()
-        logging.info(msg)
         drawblack.text((10, 0), msg, font = font20, fill = 0)
         drawred.text((10, 52), msg, font=font20, fill=0)
-        epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
+        epd.display(epd.getbuffer(HRYimage), epd.getbuffer(HBlackimage))
 
         epd.sleep()
 
     except IOError as e:
-        logging.info(e)
+        print(e)
 
     except KeyboardInterrupt:
         epd2in13b_V3.epdconfig.module_exit()
